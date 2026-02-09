e18b9cd17686a76c5b702126c0ed87138e52e7de
Mayank Verma <159456206+Mayank-121@users.noreply.github.com>
2026-02-09 22:00:29 +0530

    Update 02_array.js

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -9,23 +9,23 @@ const dc_heros = ["superman", "flash", "batman"]
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
+const all_new_heros = [...marvel_heros, ...dc_heros]  //spread operator . we can add two or more than two arrays in a single array.
 
 // console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
-const real_another_array = another_array.flat(Infinity)
+const real_another_array = another_array.flat(Infinity)  //infinity is the depth.
 console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray("Hitesh"))    //kya ye array hai.
+console.log(Array.from("Hitesh"))        //convert it into array
+console.log(Array.from({name: "hitesh"})) // interesting  // bcoz you have to tell that whose array keys or value.
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3));  //convert to array
